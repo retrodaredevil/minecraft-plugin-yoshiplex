8b4eb64968abaca3142e668e041d8181c835e288
Joshua Shannon <retrodaredevil@gmail.com>
2021-12-25 20:53:29 -0600

    Compiling perfectly. Updated resource pack links

--- src/main/java/com/yoshiplex/ResourcePack.java
+++ src/main/java/com/yoshiplex/ResourcePack.java
@@ -1,10 +1,10 @@
 package com.yoshiplex;
 
 public enum ResourcePack {
-	HUB("https://www.dropbox.com/s/wd0aklxw1x2bhpq/YoshiPlex%20Official%20Resource%20Pack.zip?dl=1".toLowerCase()),
-	MARIOCIRCUIT("https://www.dropbox.com/s/o37kwkxj5jltcvs/YP%20MK%20MarioCircuit%20v1.zip?dl=1".toLowerCase()),
-	RAINBOWROAD("https://www.dropbox.com/s/yl9qw87lgelhw2m/YP%20MK%20RainbowRoad%20v1.zip?dl=1".toLowerCase()),
-	BOWSERCASTLE("https://www.dropbox.com/s/nqh4ohavrhp4bza/YP%20MK%20GBA%20Bowser%20Castle%202%20v1.zip?dl=1".toLowerCase()),
+	HUB("https://github.com/retrodaredevil/public-files/raw/master/minecraft/resourcepacks/yp/YoshiPlex%20Official%20Resource%20Pack.zip"),
+	MARIOCIRCUIT("https://github.com/retrodaredevil/public-files/raw/master/minecraft/resourcepacks/yp/YP%20MK%20MarioCircuit%20v1.zip"),
+	RAINBOWROAD("https://github.com/retrodaredevil/public-files/raw/master/minecraft/resourcepacks/yp/YP%20MK%20RainbowRoad%20v1.zip"),
+	BOWSERCASTLE("https://github.com/retrodaredevil/public-files/raw/master/minecraft/resourcepacks/yp/YP%20MK%20GBA%20Bowser%20Castle%202%20v1.zip"),
 	BLANK("https://www.dropbox.com/s/665kacapeob8yhi/blankpack.zip?dl=1".toLowerCase()), 
 	NOAPPLY(null);
 	
